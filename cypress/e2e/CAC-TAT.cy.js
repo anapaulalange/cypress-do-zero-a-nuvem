1c3fefd714585054ce4ab4a186ab2166a53f4342
Ana Paula <anapaula.lange@gmail.com>
2025-05-21 22:37:58 -0300

    exercicicios finais

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -85,5 +85,17 @@
 
         cy.get('.error').should('be.visible')
       })
+
+      it('seleciona um produto (YouTube) por seu texto', () => {
+        cy.get('#product').select('YouTube').should('have.value', 'youtube')
+      })  
+
+      it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+        cy.get('#product').select('mentoria').should('have.value', 'mentoria')
+      }) 
+
+      it('seleciona um produto (Blog) por seu índice', () => {
+        cy.get('#product').select(1).should('have.value', 'blog')
+      }) 
       
     })
