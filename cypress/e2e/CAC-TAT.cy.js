36dc7c222f454d18f5ab7ff126c2c45bfc5cef96
Ana Paula <anapaula.lange@gmail.com>
2025-05-21 23:31:41 -0300

    exercicios extras

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -6,45 +6,66 @@
       })
       
       it('verifica o título da aplicação', () => {
-        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+        cy.title()
+        .should('be.equal','Central de Atendimento ao Cliente TAT')
       })
 
 
       it('preenche os campos obrigatórios e envia o formulário', () => {
         const longText = Cypress._.repeat('abcdefghijklmnopqrstuvxz', 10)
 
-        cy.get('#firstName').type('Ana Paula')
-        cy.get('#lastName').type('Lange Gomes')
-        cy.get('#email').type('anapaula.lange@gmail.com')
-        cy.get('#phone').type('41998764532')
-        cy.get('#open-text-area').type(longText, { delay: 0})
-        cy.get('button[type="submit"]').click()
+        cy.get('#firstName')
+        .type('Ana Paula')
+        cy.get('#lastName')
+        .type('Lange Gomes')
+        cy.get('#email')
+        .type('anapaula.lange@gmail.com')
+        cy.get('#phone')
+        .type('41998764532')
+        cy.get('#open-text-area')
+        .type(longText, { delay: 0})
+        cy.get('button[type="submit"]')
+        .click()
 
         cy.get('.success').should('be.visible')
       })
 
       it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
-        cy.get('#firstName').type('Ana Paula')
-        cy.get('#lastName').type('Lange Gomes')
-        cy.get('#email').type('anapaula.lange.gmail.com')
-        cy.get('#phone').type('41998764532')
-        cy.get('#open-text-area').type('Test')
-        cy.get('button[type="submit"]').click()
+        cy.get('#firstName')
+        .type('Ana Paula')
+        cy.get('#lastName')
+        .type('Lange Gomes')
+        cy.get('#email')
+        .type('anapaula.lange.gmail.com')
+        cy.get('#phone')
+        .type('41998764532')
+        cy.get('#open-text-area')
+        .type('Test')
+        cy.get('button[type="submit"]')
+        .click()
 
         cy.get('.error').should('be.visible')
       })
 
       it('campo telefone continua vazio quando preenchido com um valor não numérico', () => {
-        cy.get('#phone').type('abcde').should('have.value','')
+        cy.get('#phone')
+        .type('abcde')
+        .should('have.value','')
       })  
 
       it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
-        cy.get('#firstName').type('Ana Paula')
-        cy.get('#lastName').type('Lange Gomes')
-        cy.get('#email').type('anapaula.lange@gmail.com')
-        cy.get('#phone-checkbox').click()
-        cy.get('#open-text-area').type('Test')
-        cy.get('button[type="submit"]').click()
+        cy.get('#firstName')
+        .type('Ana Paula')
+        cy.get('#lastName')
+        .type('Lange Gomes')
+        cy.get('#email')
+        .type('anapaula.lange@gmail.com')
+        cy.get('#phone-checkbox')
+        .click()
+        cy.get('#open-text-area')
+        .type('Test')
+        cy.get('button[type="submit"]')
+        .click()
 
         cy.get('.error').should('be.visible')
       })
@@ -56,16 +77,21 @@
         cy.get('#lastName').type('Lange Gomes').should('have.value','Lange Gomes')
         .clear()
         .should('have.value','')
-        cy.get('#email').type('anapaula.lange@gmail.com').should('have.value','anapaula.lange@gmail.com')
+        cy.get('#email')
+        .type('anapaula.lange@gmail.com')
+        .should('have.value','anapaula.lange@gmail.com')
         .clear()
         .should('have.value','')
-        cy.get('#phone').type('41998765432').should('have.value','41998765432')
+        cy.get('#phone')
+        .type('41998765432')
+        .should('have.value','41998765432')
         .clear()
         .should('have.value','')
       })
 
       it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
-        cy.get('button[type="submit"]').click()
+        cy.get('button[type="submit"]')
+        .click()
 
         cy.get('.error').should('be.visible')
       })  
@@ -77,25 +103,70 @@
       })  
 
       it('envia mensagem de erro ao submeter o formulario com um email com formatacao invalida', () => {
-        cy.get('#firstName').type('Ana Paula')
-        cy.get('#lastName').type('Lange Gomes')
-        cy.get('#email').type('anapaula.lange.gmail.com')
-        cy.get('#open-text-area').type('Test')
-        cy.contains('button', 'Enviar').click()
+        cy.get('#firstName')
+        .type('Ana Paula')
+        cy.get('#lastName')
+        .type('Lange Gomes')
+        cy.get('#email')
+        .type('anapaula.lange.gmail.com')
+        cy.get('#open-text-area')
+        .type('Test')
+        cy.contains('button', 'Enviar')
+        .click()
 
         cy.get('.error').should('be.visible')
       })
 
       it('seleciona um produto (YouTube) por seu texto', () => {
-        cy.get('#product').select('YouTube').should('have.value', 'youtube')
+        cy.get('#product').select('YouTube')
+        .should('have.value', 'youtube')
       })  
 
       it('seleciona um produto (Mentoria) por seu valor (value)', () => {
-        cy.get('#product').select('mentoria').should('have.value', 'mentoria')
+        cy.get('#product').select('mentoria')
+        .should('have.value', 'mentoria')
       }) 
 
       it('seleciona um produto (Blog) por seu índice', () => {
-        cy.get('#product').select(1).should('have.value', 'blog')
+        cy.get('#product')
+        .select(1)
+        .should('have.value', 'blog')
       }) 
       
+      it('marca o tipo de atendimento "Feedback', () => {
+        cy.get('input[type="radio"][value="feedback"]').check().should('be.checked')
+      }) 
+
+      it('marca cada tipo de atendimento', () => {
+        cy.get('input[type="radio"]')
+        .each(typeOfService => {
+          cy.wrap(typeOfService)
+          .check()
+          .should('be.checked')
+        })  
+      }) 
+
+      it('marca ambos checkboxes, depois desmarca o último', () => {
+        cy.get('input[type="checkbox"]')
+        .check()
+        .should('be.checked')
+        .last()
+        .uncheck()
+        .should('not.be.checked')
+      })
+
+      it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+         cy.get('#firstName')
+        .type('Ana Paula')
+        cy.get('#lastName')
+        .type('Lange Gomes')
+        cy.get('#email')
+        .type('anapaula.lange@gmail.com')
+        cy.get('#phone-checkbox')
+        .check()
+        cy.contains('button', 'Enviar')
+        .click()
+
+        cy.get('.error').should('be.visible')
+      })
     })
